77e6d619313857dd03afbca3139a974e5720df4e
Ashok <ashok.chintakayala30@gmail.com>
2025-02-03 23:10:55 +0530

    Your

--- src/main/java/Chinthakayala/Spring1/Senior.java
+++ src/main/java/Chinthakayala/Spring1/Senior.java
@@ -2,13 +2,30 @@ package Chinthakayala.Spring1;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+record Person(String name, int age,Address address) { };
+record  Address (int id, String street, String city) { };
+    @Configuration
+    public class Senior {
+        @Bean
+        public String name() {
+            return "Ashok";
+        }
+        @Bean
+        public int age() {
+            return 18;
+        }
 
-@Configuration
-public class Senior {
-    @Bean
-    public String run()
-    {
-        return " Succesfully Done";
+        @Bean
+        public Person person() {
+            return new Person("Vamshi",25, new Address(6,"Cpi office","Khammam"));
+        }
+        @Bean
+        public Person person2methodcall() {
+            return new Person(name(),age(),addres());
+        }
+        @Bean (name="address2")
+        public Address addres() {
+            return new Address(6,"Cpi office","Khammam");
+        }
     }
 
-}
--- src/main/java/Chinthakayala/Spring1/Spring1Application.java
+++ src/main/java/Chinthakayala/Spring1/Spring1Application.java
@@ -7,12 +7,17 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 @SpringBootApplication
 public class Spring1Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Spring1Application.class, args);
+	public static void main(String[] args)
+
 		{
 			var Context=new AnnotationConfigApplicationContext(Senior.class);
-			System.out.println(Context.getBean("run"));
+			System.out.println(Context.getBean("name"));
+			System.out.println(Context.getBean("age"));
+			System.out.println(Context.getBean("person"));
+			System.out.println(Context.getBean("person2methodcall"));
+			//System.out.println(Context.getBean("addres"));
+			System.out.println(Context.getBean("address2"));
 		}
 	}
 
-}
+

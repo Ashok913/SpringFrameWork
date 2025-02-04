adc0fab0c75b63a671bd8cb958b0da8e8259ea69
Ashok <ashok.chintakayala30@gmail.com>
2025-02-05 03:09:15 +0530

    Your

--- src/main/java/Chinthakayala/Spring1/Spring1Application.java
+++ src/main/java/Chinthakayala/Spring1/Spring1Application.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.Arrays;
+
 @SpringBootApplication
 public class Spring1Application {
 
@@ -13,10 +15,12 @@ public class Spring1Application {
 			var Context=new AnnotationConfigApplicationContext(Senior.class);
 			System.out.println(Context.getBean("name"));
 			System.out.println(Context.getBean("age"));
-			System.out.println(Context.getBean("person"));
-			System.out.println(Context.getBean("person2methodcall"));
+			System.out.println(Context.getBean(Person.class));
+			//System.out.println(Context.getBean("person2methodcall"));
 			//System.out.println(Context.getBean("addres"));
-			System.out.println(Context.getBean("address2"));
+			System.out.println(Context.getBean(Address.class));
+			//Arrays.stream(Context.getBeanDefinitionNames()).forEach(System.out::println);
+
 		}
 	}
 
